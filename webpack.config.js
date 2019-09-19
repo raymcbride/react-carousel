a95eab1567a2b7e2790e4f7d8daed5e09309e757
Ray McBride <ray@raymcbride.com>
2019-09-19 11:45:37 +0100

    update packages

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     './app/index.js'
   ],
   module: {
-    loaders: [
+    rules: [
       {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
       {test: /\.css$/, loader: "style-loader!css-loader"}
     ]
